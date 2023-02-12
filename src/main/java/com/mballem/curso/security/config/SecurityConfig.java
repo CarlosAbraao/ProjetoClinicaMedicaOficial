0d3e84a5dc7d7ea0bb3aaac01023c062b24b7b75
Carlos Abraao <carloschavesnd@hotmail.com>
2023-02-12 14:10:58 -0300

    segurança basica da aplicacao

---
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.mballem.curso.security.config;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+                    // CONFIGURANDO SPRING NA VERSÃO ANTIGA
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
