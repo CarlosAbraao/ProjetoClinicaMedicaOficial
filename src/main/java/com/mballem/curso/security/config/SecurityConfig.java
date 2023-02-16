0470e375ec36ba620fd306496943062ae3bded5a
Carlos Abraao <carloschavesnd@hotmail.com>
2023-02-16 08:30:41 -0300

    criptografia de senha

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,12 +1,23 @@
 package com.mballem.curso.security.config;
 
 
+import com.mballem.curso.security.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // CRIPTOGRAFANDO A SENHA DO USUARIO
+    @Autowired
+    private UsuarioService service;
+
+
+
                     // CONFIGURANDO SPRING NA VERSÃO ANTIGA
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,5 +42,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutSuccessUrl("/");
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+
+        auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
