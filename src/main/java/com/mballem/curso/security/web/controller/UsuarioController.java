35443770311f14540de991f630395f0caffe27ca
Carlos Abraao <carloschavesnd@hotmail.com>
2023-02-20 18:10:33 -0300

    conversor e cadastro de usuario

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,16 +1,21 @@
 package com.mballem.curso.security.web.controller;
 
 
+import com.mballem.curso.security.domain.Perfil;
 import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.Response;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("u")
@@ -43,8 +48,32 @@ private UsuarioService service;
         return ResponseEntity.ok(service.buscarTodos(request));
 
 
-
-
     }
+
+
+    // SALVAR UM CADASTRO DE USUARIO
+
+    @PostMapping("/cadastro/salvar")
+    public String salvarUsuarios(Usuario usuario, RedirectAttributes attr){
+
+        List<Perfil> perfis = usuario.getPerfis();
+
+        if (perfis.size() > 2 ||
+
+                perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L))) ||
+                perfis.contains(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
+
+            attr.addFlashAttribute("falha", "Paciente não pode ser Amin e/ou Médioco. ");
+            attr.addFlashAttribute("usuario", usuario);
+
+        }   else{
+                  service.savarUsuario(usuario);
+                  attr.addFlashAttribute("sucesso", "Operação realizada com sucesso!");
+            }
+
+
+        return "redirect:/u/novo/cadastro/usuario";
+    }
+
 
 }
